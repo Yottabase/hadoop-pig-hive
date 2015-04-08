1694c142ad5a1e5687e8dc7db1c9dc3537e5ce52
Christian Vadalà <chr.vadala@gmail.com>
2015-04-08 10:38:41 +0200

    aggiunge GlobalMain che esegue uno dopo l'altro i job configurati

---
+++ src/org/yottabase/billing/GlobalMain.java
@@ -0,0 +1,28 @@
+package org.yottabase.billing;
+
+import java.io.File;
+
+import org.yottabase.utils.FileSystem;
+
+public class GlobalMain {
+
+	public static void main(String[] args) throws Exception {
+		
+		String inputPath, outputPath;
+		
+		if(args.length == 2){
+			inputPath = args[0];
+			outputPath = args[1];	
+		}else{
+			inputPath = "data/generator/sample/";
+			outputPath = "data/output/";
+		}
+		
+		FileSystem.deleteDirectory(new File(outputPath));
+		
+		org.yottabase.billing.es1.onereducer.Main.runJob(inputPath, outputPath + "/es1_simple_billing_onered");
+		
+		org.yottabase.billing.es2.Main.runJob(inputPath, outputPath + "/es2_quarter_aggregation");
+	}
+
+}
