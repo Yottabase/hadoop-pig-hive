9fb6c7ae9ddfc4ae84f3cb323196ceebc6572ccb
hduser <hduser@leonardo-K53SD>
2015-04-21 14:13:54 +0200

    Abbozza soluzione esercizio opzionale 2

---
+++ src/org/yottabase/billing/hive/udf/PowersetUDF.java
@@ -0,0 +1,77 @@
+package org.yottabase.billing.hive.udf;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+public class PowersetUDF extends UDF {
+	
+	private static final int MAX_SUBSET_SIZE = 4;
+	
+	public Text evaluate(Text prods) {
+		/* Tokenize input line */
+		StringTokenizer tokenizer = new StringTokenizer(prods.toString(), ",");
+		
+		/* Skip record date */
+		tokenizer.nextToken();
+
+		/* Collect unique items sorted by name */
+		Set<Text> items = new TreeSet<Text>();
+		while ( tokenizer.hasMoreTokens() )
+			items.add(new Text( tokenizer.nextToken() ));
+		
+		/* Compute items powerset */
+		Set<Set<Text>> powerset = powersetOfMaxDimension(items, MAX_SUBSET_SIZE);
+		
+		/* Iterate over subsets and emit each as string key */
+		for (Set<Text> subset : powerset)
+			if ( !subset.isEmpty() ) {
+				String subsetStr = new String();
+				Iterator<Text> iterator = subset.iterator();
+				
+				while ( iterator.hasNext() )
+					subsetStr += iterator.next().toString() + " ";
+				
+//				context.write(new Text(subseq), ONE);
+			}
+		
+	}
+	
+	/**
+	 * Computes the powerset of the set given as input parameter,
+	 * that is the set containing all subsets of the input set 
+	 * (each of which has dimension not grater than maxDim)
+	 * 
+	 * @param originalSet is the input set
+	 * @param maxDim is the maximum dimension allowed for a powerset element
+	 * @return the powerset of the input set, with elements not larger than maxDim
+	 */
+	private static Set<Set<Text>> powersetOfMaxDimension(Set<Text> originalSet, int maxDim) {
+		Set<Set<Text>> powerset = new HashSet<Set<Text>>();
+		if ( originalSet.isEmpty() ) {
+			powerset.add( new TreeSet<Text>() );
+			return powerset;
+		}
+		
+		List<Text> list = new ArrayList<Text>(originalSet);
+		Text head = list.get(0);
+		Set<Text> rest = new TreeSet<Text>( list.subList(1, list.size()) );
+		for ( Set<Text> set : powersetOfMaxDimension(rest, maxDim) )
+			if ( set.size() < maxDim ) {
+				Set<Text> newSet = new TreeSet<Text>();
+				newSet.add(head);
+				newSet.addAll(set);
+				
+				powerset.add(newSet);
+				powerset.add(set);
+			}
+		return powerset;
+	}
+}
