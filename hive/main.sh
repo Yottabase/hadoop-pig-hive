16315915d6cdcc66306c6e16f5322a8b85b6b7a6
Christian Vadalà <chr.vadala@gmail.com>
2015-04-21 17:18:56 +0200

    ottimizza main di hive

--- hive/main.sh
+++ hive/main.sh
@@ -1,19 +1,23 @@
 #! /usr/bin/env bash
 
+/* CONFIG */
+INPUT="../data/generator/sample/esempio.txt"
+OUTPUT="../data/output/hive/"
+EXTLIB="../target/bigdata-0.0.1-BETA.jar"
+/* END CONFIG */
+
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 
-rm -Rv ../data/output/hive/*
-
 echo "STARTING ES1..."
-hive -f es1.hql -hiveconf INPUT="../data/generator/sample/esempio.txt" -hiveconf OUTPUT="../data/output/hivea/"
+hive -f es1.hql -hiveconf INPUT=$INPUT -hiveconf OUTPUT=$OUTPUT
 
 echo "STARTING ES2..."
-hive -f es2.hql -hiveconf INPUT="../data/generator/sample/esempio.txt" -hiveconf OUTPUT="../data/output/hiveb/"
+hive -f es2.hql -hiveconf INPUT=$INPUT -hiveconf OUTPUT=$OUTPUT
 
 echo "STARTING ES3..."
-hive -f es3.hql -hiveconf INPUT="../data/generator/sample/esempio.txt" -hiveconf OUTPUT="../data/output/hivec/"
+hive -f es3.hql -hiveconf INPUT=$INPUT -hiveconf OUTPUT=$OUTPUT
 
 echo "STARTING OPT1..."
-hive -f opt1.hql -hiveconf INPUT="../data/generator/sample/esempio.txt" -hiveconf OUTPUT="../data/output/hived/"
+hive -f opt1.hql -hiveconf INPUT=$INPUT -hiveconf OUTPUT=$OUTPUT
